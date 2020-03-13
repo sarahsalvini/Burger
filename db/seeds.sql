a86ccbc39dd91a2734ad5e9a6ddba7a467cf8573
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-03-13 11:13:52 -0500

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
